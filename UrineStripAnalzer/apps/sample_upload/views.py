ac5eb85a84017f6ce01f164c575e8d416a1ec346
Dhruv Khanna <dhruvkhanna930@gmail.com>
2024-06-25 23:49:02 +0530

    Added image processing model and set views.py

--- UrineStripAnalzer/apps/sample_upload/views.py
+++ UrineStripAnalzer/apps/sample_upload/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import UploadedImage
+from .serializers import UploadedImageSerializer
+from .image_process import analyze_colors
 
-# Create your views here.
+@api_view(['POST'])
+def upload_image_api(request):
+    if request.method == 'POST':
+        serializer = UploadedImageSerializer(data=request.data)
+        if serializer.is_valid():
+            uploaded_image = serializer.save()
+            colors = analyze_colors(uploaded_image.image.path)
+            return Response({'colors': colors}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
